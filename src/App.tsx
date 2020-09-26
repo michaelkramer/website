3769d33b2c07553d44b43c43cef526b98cc1f9e3
Michael Kramer <michaelrkramer@gmail.com>
2020-09-25 21:32:37 -0400

    basic

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>Michael Kramer</div>
+        <code>Simple website to display what I have done.</code>
+      </header>
+    </div>
+  );
+}
+
+export default App;
