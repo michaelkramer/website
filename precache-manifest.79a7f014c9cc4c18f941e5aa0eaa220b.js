98713d642a6b15d69bee39f2398a21a08a5143c1
Michael Kramer <michaelrkramer@gmail.com>
2020-09-25 21:57:21 -0400

    Updates

---
+++ precache-manifest.79a7f014c9cc4c18f941e5aa0eaa220b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71a358cf445419a15e1d0540988c0c20",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "665c95002e149a68b7f5",
+    "url": "/website/static/css/main.25777861.chunk.css"
+  },
+  {
+    "revision": "6cfb111f17a5d6a30852",
+    "url": "/website/static/js/2.03513662.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/website/static/js/2.03513662.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "665c95002e149a68b7f5",
+    "url": "/website/static/js/main.9d70e5f9.chunk.js"
+  },
+  {
+    "revision": "9c23e00f99dd7916e537",
+    "url": "/website/static/js/runtime-main.9b825fe6.js"
+  }
+]);
