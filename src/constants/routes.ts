b1f15299f08af4235d5ba60cdef2691b9e710cec
Michael Kramer <michaelrkramer@gmail.com>
2020-09-27 12:56:01 -0400

    Setting the PUBLIC_URL in routes

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,5 +1,5 @@
-export const HOME = "/%PUBLIC_URL%/";
-export const RESUME = "/%PUBLIC_URL%/resume";
-export const RLS = "/%PUBLIC_URL%/rls";
-export const SIDE = "/%PUBLIC_URL%/side";
-export const NOTES = "/notes";
+export const HOME = `${process.env.PUBLIC_URL}/`;
+export const RESUME = `/${process.env.PUBLIC_URL}/resume`;
+export const RLS = `/${process.env.PUBLIC_URL}/rls`;
+export const SIDE = `/${process.env.PUBLIC_URL}/side`;
+export const NOTES = `/${process.env.PUBLIC_URL}/notes`;
